d918f945489a2a868a777b349ee400079d638464
arekko <you@example.com>
2019-01-21 07:11:37 +0200

    added confirm register feature

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -4,6 +4,8 @@ import { Arg, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
 import { User } from "../../entity/User";
 import { RegisterInput } from "./register/RegisterInput";
 import { isAuth } from "../middleware/isAuth";
+import { sendEmail } from "../utils/sendEmail";
+import { createConfirmationUrl } from "../utils/createConfirmationUrl";
 
 @Resolver()
 export class RegisterResolver {
@@ -29,6 +31,8 @@ export class RegisterResolver {
       email,
       password: hashPassword
     }).save();
+
+    await sendEmail(email, await createConfirmationUrl(user.id));
 
     return user;
   }
